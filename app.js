d90135ad327d859d3bafc1ef83f32c8e656c5b05
Md. Ehtisham Farooqui <mefxe01@gmail.com>
2021-02-13 20:13:49 +0530

    'testing the sintax'

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 var container = document.getElementById('container');
-function countdown(initial, final, interval) {
-    if (initial === void 0) { initial = 10; }
-    if (final === void 0) { final = 0; }
-    if (interval === void 0) { interval = 1; }
-    var current = initial;
-    while (current > final) {
-        container.innerHTML = current.toString();
-        current -= interval;
-    }
-}
+var todo = {
+    id: 123,
+    name: 'pickup the cloths',
+    completed: true
+};
+var displayName = "Todo #" + todo.id;
--- app.ts
+++ app.ts
@@ -1,9 +1,8 @@
 var container = document.getElementById('container');
-
-function countdown (initial=10, final=0, interval=1){
-  var current = initial 
-  while(current>final){
-    container.innerHTML = current.toString()
-    current -= interval
-  }
+let todo = {
+  id: 123,
+  name: 'pickup the cloths',
+  completed: true
 }
+
+let displayName = `Todo #${todo.id}`
